478184ec432fc9b64f3f3be690e9576326026495
Hamadalishah <hammadalis459@gmail.com>
2025-09-15 15:48:55 +0500

    add media file here

--- tribune-blog-cms-main/src/payload-types.ts
+++ tribune-blog-cms-main/src/payload-types.ts
@@ -133,6 +133,6 @@ export interface PayloadMigration {
 }
 
 
-declare module 'payload' {
-  export interface GeneratedTypes extends Config {}
-}
+// declare module 'payload' {
+//   export interface GeneratedTypes extends Config {}
+// }
